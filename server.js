a2c5d915b85cc3150b4ecdf4d5a8580fe9aaddf5
ShosukeDoi <doiverson.canada@gmail.com>
2021-09-15 13:41:34 -0700

    update

---
+++ server.js
@@ -0,0 +1,35 @@
+const Pusher = require('pusher');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const pusher = new Pusher({
+    appId: process.env.APP_ID,
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    cluster: "us3",
+    useTLS: true
+});
+
+app.set('PORT', process.env.PORT || 5000);
+
+app.post('/message', (req, res) => {
+    const payload = req.body;
+    pusher.trigger('my-channel', 'my-event', payload).then(res => {
+        console.log(res);
+    }).catch((error) => {
+        console.log(error)
+    });
+    res.send(payload);
+});
+
+app.listen(app.get('PORT'), () => console.log('Listening at ' + app.get('PORT')));
+
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Pusher from 'pusher-js';
+import ChatList from './components/ChatList';
+import ChatBox from './components/ChatBox';
+
+
+function App() {
+
+  const [ text, setText ] = useState('');
+  const [ username, setUsername ] = useState('');
+  const [ chats, setChats ] = useState([]);
+
+  useEffect(() => {
+    const username = window.prompt('Username: ', 'Anonymous');
+    setUsername(username);
+    const pusher = new Pusher(process.env.REACT_APP_KEY, {
+      cluster: 'us3',
+      encrypted: true
+    });
+
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', data => {
+      setChats([...chats, data]);
+      setText('');
+    });
+  }, []);
+
+  const handleTextChange = (e) => {
+    if (e.keyCode === 13) {
+      const payload = {
+        username: username,
+        message: text
+      };
+      axios.post('http://localhost:5000/message', payload);
+    } else {
+      setText(e.target.value);
+    }
+  }
+
+  console.log(process.env)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React-Pusher Chat</h1>
+      </header>
+      <section>
+        <ChatList chats={chats} />
+        <ChatBox
+            text={text}
+            username={username}
+            handleTextChange={handleTextChange}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
